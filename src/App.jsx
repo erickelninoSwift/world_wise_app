839096d1ec33ab07ccb7f5d4e0b4c860785c7d49
Erick El nino <erick.tshimbombo@yahoo.com>
2023-10-02 04:02:26 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import "./index.css";
 function App() {
   return (
     <>
-      <h1>WorldWise</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          {/* <Route path="Products" element={<Products />} />
+          <Route path="Pricing" element={<Pricing />} />
+          <Route path="Applyout" element={<Applyout />} />
+          <Route path="*" element={<PageNotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
